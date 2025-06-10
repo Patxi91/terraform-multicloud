447a5d73f8ef840bdaf7ce868ab06b267b002633
Patxi Oyaga <oyaga888@gmail.com>
2025-06-10 17:02:47 +0200

    initial tf configuration

---
+++ 8-KubernetesWebApp/aks/aks-cluster.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+# AKS Resource Group
+resource "azurerm_resource_group" "aks_terraform_rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    environment = var.environment_tag
+  }
+}
+
+# Azure Kubernetes Service (AKS) Cluster
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.aks_terraform_rg.location
+  resource_group_name = azurerm_resource_group.aks_terraform_rg.name
+  dns_prefix          = var.aks_dns_prefix
+
+  default_node_pool {
+    name            = "systempool"
+    node_count      = var.node_count
+    vm_size         = var.vm_size
+    os_disk_size_gb = var.os_disk_size_gb
+  }
+
+  service_principal {
+    client_id     = var.app_id
+    client_secret = var.client_secret
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    environment = var.environment_tag
+  }
+}
---
+++ 8-KubernetesWebApp/aks/variables.tf
@@ -0,0 +1,60 @@
+variable "resource_group_name" {
+  description = "The name of the Azure Resource Group to create."
+  type        = string
+  default     = "aks-terraform-rg"
+}
+
+variable "location" {
+  description = "The Azure region where the resources will be deployed (e.g., 'Switzerland North' or 'northeurope')."
+  type        = string
+  default     = "northeurope"
+}
+
+variable "aks_cluster_name" {
+  description = "The name of the Azure Kubernetes Cluster (AKS)."
+  type        = string
+  default     = "my-aks-cluster"
+}
+
+variable "aks_dns_prefix" {
+  description = "The DNS prefix for the AKS cluster. Must be unique within the Azure region."
+  type        = string
+  default     = "my-aks-dns"
+}
+
+variable "node_count" {
+  description = "The number of nodes in the default node pool. Set to 1 for the smallest configuration."
+  type        = number
+  default     = 1
+}
+
+variable "vm_size" {
+  description = "The size of the Virtual Machine for the AKS nodes. 'Standard_B1s' is one of the smallest and cheapest available."
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "os_disk_size_gb" {
+  description = "The OS Disk size in GB for the AKS nodes. 30GB is typically the minimum required for AKS."
+  type        = number
+  default     = 30 # Minimum recommended OS disk size for AKS
+}
+
+variable "app_id" {
+  description = "The Client ID (App ID) for the Service Principal used by AKS."
+  type        = string
+  # IMPORTANT: Do not hardcode sensitive values here. Use a .tfvars file or environment variables.
+}
+
+variable "client_secret" {
+  description = "The Client Secret (Password) for the Service Principal used by AKS."
+  type        = string
+  sensitive   = true # Marks the variable as sensitive to prevent it from being shown in plan/apply output.
+  # IMPORTANT: Do not hardcode sensitive values here. Use a .tfvars file or environment variables.
+}
+
+variable "environment_tag" {
+  description = "Environment tag (e.g., 'Dev', 'Prod', 'Demo')."
+  type        = string
+  default     = "Demo"
+}
